1d24aa46cd9e5bc642868a495cc920c277f9ef24
Antonin Rouxel <antonin.rouxel44@outlook.fr>
2023-10-05 23:16:16 +0200

    Update

---
+++ MedicAppToolsDatasets.Tools/Program.cs
@@ -0,0 +1,24 @@
+﻿string url = "https://base-donnees-publique.medicaments.gouv.fr/telechargement.php?fichier=CIS_bdpm.txt";
+
+using (HttpClient client = new HttpClient())
+{
+    HttpResponseMessage response = await client.GetAsync(url);
+    if (response.IsSuccessStatusCode)
+    {
+        if (response.Content.Headers.ContentType != null)
+        {
+            response.Content.Headers.ContentType.CharSet = "latin1";
+        }
+        string result = await response.Content.ReadAsStringAsync();
+        string[] lines = result.Split('\n');
+        foreach (string line in lines)
+        {
+            string[] data = line.Split('\t');
+            foreach (string item in data)
+            {
+                Console.Write(item + "|");
+            }
+            Console.WriteLine();
+        }
+    }
+}
