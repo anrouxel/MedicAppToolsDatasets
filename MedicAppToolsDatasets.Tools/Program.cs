628f9771ebd8b474220654065e6a0daa6b4ce6d6
Antonin Rouxel <antonin.rouxel44@outlook.fr>
2023-10-08 10:59:13 +0200

    Update

--- MedicAppToolsDatasets.Tools/Program.cs
+++ MedicAppToolsDatasets.Tools/Program.cs
@@ -10,15 +10,6 @@ using (HttpClient client = new HttpClient())
             response.Content.Headers.ContentType.CharSet = "latin1";
         }
         string result = await response.Content.ReadAsStringAsync();
-        string[] lines = result.Split('\n');
-        foreach (string line in lines)
-        {
-            string[] data = line.Split('\t');
-            foreach (string item in data)
-            {
-                Console.Write(item + "|");
-            }
-            Console.WriteLine();
-        }
+        Console.WriteLine(result);
     }
 }
